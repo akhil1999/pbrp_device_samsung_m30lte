c5862f84714619b4506e4015461b7e98e028a03b
akhil1999 <akhileshbhuvanendra@gmail.com>
2020-12-12 20:04:10 +0530

    Change vendor from omni to pb

--- omni_m30lte.mk
+++ omni_m30lte.mk
@@ -5,7 +5,7 @@ PRODUCT_RELEASE_NAME := m30lte
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := m30lte
